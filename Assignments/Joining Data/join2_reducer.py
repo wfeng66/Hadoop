2f9b055ba47ece452bbcbbf75af0d6de3ad9419e
wfeng66 <weicongf73@gmail.com>
2020-06-28 11:40:37 -0700

    new file:   Assignments/Joining Data/Joining Data Readme.docx 	new file:   Assignments/Joining Data/Readme.docx 	modified:   Assignments/Joining Data/join2_reducer.py 	modified:   Assignments/Joining Data/part-00000 	new file:   Assignments/Running Wordcount with Hadoop streaming/Readme.docx

--- Assignments/Joining Data/join2_reducer.py
+++ Assignments/Joining Data/join2_reducer.py
@@ -2,18 +2,26 @@
 import sys
 
 show_cnts_to_output = [] #an empty list of day counts for a given word
+prev_show = ""
+prev_value = ""
+
 
 for line in sys.stdin:
     line       = line.strip()       #strip out carriage return
-    key_value  = line.split('\t')   #split line, into key and value, returns a list   
+    key_curr_value  = line.split('\t')   #split line, into key and curr_value, returns a list   
 
-    curr_show  = key_value[0]       #key is first item in list, indexed by 0
-    value   = key_value[1]          #value is 2nd item
-
-    if (value.isdigit()):           #After sorting by hadoop, the channel would sit on the end of show lists
-        show_cnts_to_output.append(value)   #If the value is digits, push it to the list for sum later
-    else:                           #If the value isn't digits, it means reaching the end of specific show list
-        total_vws = sum(list(map(int, show_cnts_to_output)))  #count the sum of the list
-        print('{0} {1}'.format(curr_show, total_vws))
+    curr_show  = key_curr_value[0]       #key is first item in list, indexed by 0
+    curr_value   = key_curr_value[1]          #curr_value is 2nd item
+    
+    if(prev_show!=curr_show):
+        if(prev_value[:3]=='ABC'):
+            total_vws = sum(list(map(int, show_cnts_to_output)))  #count the sum of the list
+            print('{0} {1}'.format(curr_show, total_vws))
         show_cnts_to_output=[]      #reset the list for new show
+        show_cnts_to_output.append(curr_value)   #push the value to the list for sum later
+    else:
+        if (curr_value.isdigit()):           #After sorting by hadoop, the channel would sit on the end of show lists
+            show_cnts_to_output.append(curr_value)   #push the value to the list for sum later
+    prev_show = curr_show                    #reset the prev_show    
+    prev_value = curr_value                  #reset the prev_value
     
